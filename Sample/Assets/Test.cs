9a260d3a89039446c79198f33bf460580974526c
oosawagithub <newstation1215@gmail.com>
2020-01-30 18:35:00 +0900

    クリックしたら移動するスクリプトの作成

---
+++ Sample/Assets/Test.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    private int hp = 100;
+    private int power = 50;
+
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    public void Damage(int damage)
+    {
+        this.hp -= damage;
+        Debug.Log(damage + "のダメージを与えた");
+    }
+}
+
+public class Test : MonoBehaviour
+{
+    void Start()
+    {
+        Player myPlayer = new Player();
+        myPlayer.Attack();
+        myPlayer.Damage(30);
+
+        Vector2 playerPos = new Vector2(3.0f, 4.0f);
+        playerPos.x += 8.0f;
+        playerPos.y += 5.0f;
+        Debug.Log(playerPos);
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ SwipeCar/Assets/CarController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    float speed = 0;
+
+    // Start is called before the first frame update
+    // Startは起動時に一回だけ実行されるもの
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //マウスがクリックされたら
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.speed = 0.2f; //初速度を設定
+        }
+
+        transform.Translate(this.speed, 0, 0); //移動 Translate(0,3,0)と書くことでY方向に3マス進む
+        this.speed  *= 0.98f; //減速
+    }
+}
